2e5601f0bae48285c46293f53df7416134d0a1c5
奔跑的面条 <1262327911@qq.com>
2023-12-17 00:12:50 +0800

    feat: iconify图标新增离线功能

--- src/packages/components/Icons/Default/Icon/index.vue
+++ src/packages/components/Icons/Default/Icon/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="go-icon-box">
-    <Icon :icon="((dataset || '') as string)" :color="color" :width="size" :rotate="rotate" />
+    <GoIconify :icon="((dataset || '') as string)" :color="color" :width="size" :rotate="rotate" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { PropType, toRefs } from 'vue'
 import { CreateComponentType } from '@/packages/index.d'
-import { Icon } from '@iconify/vue'
+import { GoIconify } from '@/components/GoIconify'
 
 const props = defineProps({
   chartConfig: {
